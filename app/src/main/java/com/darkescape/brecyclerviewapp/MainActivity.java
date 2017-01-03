6d4902621be4421843492f2ffea8ccaf812a5fd0
dark-escape <vishalwadhwa13@gmail.com>
2017-01-03 22:33:20 +0530

    Sample Features implemented

--- app/src/main/java/com/darkescape/brecyclerviewapp/MainActivity.java
+++ app/src/main/java/com/darkescape/brecyclerviewapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.darkescape.brecyclerviewapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -19,79 +21,74 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "MainActivity";
-    private BRecyclerView bview;
-    private MyAdapter ada;
+    MyAdapter ada;
+    BRecyclerView bView;
     ArrayList<String> names = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.darkescape.brecyclerviewapp.R.layout.activity_main);
-
+        setContentView(R.layout.activity_main);
         for (int i = 0; i < 20; i++) {
-            names.add(i, "Name " + (i+1));
+            names.add("Name "+(i+1));
         }
-        bview = (BRecyclerView) findViewById(com.darkescape.brecyclerviewapp.R.id.view);
-        ada = new MyAdapter(names, this, new MultiSelect(com.darkescape.brecyclerviewapp.R.menu.cam_menu));
-        bview.setLayoutManager(new LinearLayoutManager(this));
-        ada.swipeToRemove(true, bview);
-        bview.setAdapter(ada);
+
+        bView= (BRecyclerView) findViewById(R.id.view);
+        bView.setLayoutManager(new LinearLayoutManager(this));
+        ada=new MyAdapter(this,new MultiSelect(R.menu.cam_menu),names);
+        ada.swipeToRemove(true,bView);
+        bView.setAdapter(ada);
     }
 
     class MyAdapter extends BRecyclerView.Adapter<MyAdapter.ViewHolder> {
 
-        ArrayList<String> list;
-
-        MyAdapter(ArrayList<String> list, Context context, BRecyclerView.ActionModeCallback modeCallBack) {
-            super(context, modeCallBack);
-            this.list = list;
-        }
-
-
         class ViewHolder extends BRecyclerView.ViewHolder {
 
-            private TextView textView;
-
-            ViewHolder(View itemView) {
+            private TextView tv;
+            public ViewHolder(View itemView) {
                 super(itemView);
-                textView = (TextView) itemView.findViewById(com.darkescape.brecyclerviewapp.R.id.tv);
+                tv= (TextView) itemView.findViewById(R.id.tv);
             }
+        }
 
+        private ArrayList<String > name;
+
+        public MyAdapter(Context context, @NonNull BRecyclerView.ActionModeCallback modeCallBack, ArrayList<String> name) {
+            super(context, modeCallBack);
+            this.name = name;
         }
 
         @Override
-        public void positionRemovedOnSwipe(int pos) {
-            super.positionRemovedOnSwipe(pos);
-            list.remove(pos);
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            super.onBindViewHolder(holder, position);
+            holder.tv.setText(name.get(position));
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(com.darkescape.brecyclerviewapp.R.layout.item, parent, false);
-            view.setClickable(true);
+        public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view= getLayoutInflater().inflate(R.layout.item,parent,false);
             ViewHolder holder=new ViewHolder(view);
-            holder.setTouchEffect(Color.YELLOW,Color.WHITE);
+            holder.setTouchEffect(Color.GRAY,Color.WHITE);
+            //holder.setBackground(new ColorDrawable(Color.WHITE)); //--> Custom Background
+            //holder.setCheckedBackground(new ColorDrawable(Color.GREEN)); //--> background when item is selected in multi select
             triggerActionMode(holder);
             return holder;
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
-            super.onBindViewHolder(holder,position);
-            holder.textView.setText(list.get(position));
+        public void positionRemovedOnSwipe(int pos) {
+            super.positionRemovedOnSwipe(pos);
+            name.remove(pos);
         }
-
 
         @Override
         public int getItemCount() {
-            return list.size();
+            return name.size();
         }
-
 
     }
 
     class MultiSelect extends BRecyclerView.ActionModeCallback {
-
 
         MultiSelect(int menuResId) {
             super(menuResId);
@@ -99,21 +96,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
-
-            if (item.getItemId() == com.darkescape.brecyclerviewapp.R.id.del) {
-                ArrayList<String > toRemove=new ArrayList<>();
-                int[] items=ada.getSelectedIds();
-                for (int i = 0; i < items.length; i++) {
-                    toRemove.add(names.get(items[i]));
+            if (item.getItemId()==R.id.del) {
+                //perform action
+                int[] selections=ada.getSelectedIds();
+                ArrayList<String> toRemoveName=new ArrayList<>();
+                for (int i = 0; i < selections.length; i++) {
+                    toRemoveName.add(names.get(selections[i]));
                 }
-                for (String nm:
-                        toRemove) {
+
+                for (String nm :
+                        toRemoveName) {
                     names.remove(nm);
                 }
+
+                mode.finish(); //very important
+                return true;
+            } else if (item.getItemId()==R.id.share) {
+                //just an example no app takes an arraylist of strings
+                int[] selections=ada.getSelectedIds();
+                ArrayList<String> toShare=new ArrayList<>();
+                for (int i = 0; i < selections.length; i++) {
+                    toShare.add(names.get(i));
+                }
+                Intent share=new Intent(Intent.ACTION_SEND_MULTIPLE);
+                share.putStringArrayListExtra(Intent.EXTRA_TEXT,toShare);
+                share.setType("text/plain");
+                startActivity(Intent.createChooser(share,"Select:"));
                 mode.finish();
                 return true;
             }
             return false;
         }
+
     }
 }
